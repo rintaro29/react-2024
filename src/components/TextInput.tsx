1c0d7d50a2d441c4367e715eb2f0194bf03ea7f3
rintaro1029 <tomitaka1029@gmail.com>
2024-01-27 00:02:03 +0900

    vitest-setup

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const TextInput = () => {
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <input type="text" value={text} aria-label="Text Input" onChange={(e) => setText(e.target.value)} />
+      <p>Enterd Text {text}</p>
+    </div>
+  );
+};
+
+export default TextInput;
