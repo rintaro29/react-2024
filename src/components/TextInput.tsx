07b9841f4a0f99039c4d204ef71b02d54d0e3aaa
rintaro1029 <tomitaka1029@gmail.com>
2024-01-27 15:46:33 +0900

    🔧 setup eslint and prettier

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const TextInput = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
   return (
     <div>
-      <input type="text" value={text} aria-label="Text Input" onChange={(e) => setText(e.target.value)} />
+      <input
+        type="text"
+        value={text}
+        aria-label="Text Input"
+        onChange={(e) => setText(e.target.value)}
+      />
       <p>Enterd Text {text}</p>
     </div>
   );
